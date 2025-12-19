2483adf7b81a809516882522f7ad241860f0a080
Elias Tamer <eliastamer3@gmail.com>
2025-12-19 10:53:10 +0400

    started working on error handeling in  user creation controller and created a generic class for api responses

--- EcomAPI/Controllers/UsersController.cs
+++ EcomAPI/Controllers/UsersController.cs
@@ -3,6 +3,9 @@ using EcomAPI.Entities;
 using EcomAPI.Interfaces;
 using EcomAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using EcomAPI.Responses;
+using System.Net;
+
 namespace EcomAPI.Controllers
 {
     [ApiController]
@@ -17,8 +20,39 @@ namespace EcomAPI.Controllers
         [HttpPost("CreateUser")]
         public async Task<IActionResult> CreateUser([FromBody] CreateUserRequestDTO newUser)
         {
-            int id = await _usersService.CreateUser(newUser);
-            return Ok(new { id, Message = "User created." });
+            var response = new ApiResponse();
+
+            if (!ModelState.IsValid)
+            {
+                response.Success = false;
+                response.Status = HttpStatusCode.BadRequest;
+                response.Message = "Validaiton failed.";
+                response.Errors = ModelState.Values.SelectMany(v => v.Errors)
+                                  .Select(e => e.ErrorMessage)
+                                  .ToList();
+
+                return BadRequest(response);
+            }
+
+            try
+            {
+                int id = await _usersService.CreateUser(newUser);
+                response.Success = true;
+                response.Status = HttpStatusCode.OK;
+                response.Message = "User created.";
+                response.Data = new { UserCreatedId = id };
+
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                response.Success = false;
+                response.Status = HttpStatusCode.InternalServerError;
+                response.Message = "An error occurred while creating the user.";
+                response.Errors = new List<string> { ex.Message };
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, response);
+            }
         }
     }
 }
---
+++ EcomAPI/Responses/ApiResponse.cs
@@ -0,0 +1,11 @@
+namespace EcomAPI.Responses
+{
+    public class ApiResponse
+    {
+        public bool Success { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public object? Data { get; set; }
+        public List<string>? Errors { get; set; }
+    }
+}
